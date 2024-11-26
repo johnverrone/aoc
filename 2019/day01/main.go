31991e6efaea1ffdcc591d2795f975fea896526a
John Verrone <john.verrone@gmail.com>
2024-11-26 07:07:16 -0700

    2019

---
+++ 2019/day01/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const sample = `
+12
+14
+1969
+100756
+`
+
+func main() {
+	sum := 0
+	lines := strings.Split(sample, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		n := mustInt(line)
+		for {
+			n = n/3 - 2
+			if n > 0 {
+				sum += n
+			} else {
+				break
+			}
+		}
+	}
+	fmt.Println(sum)
+}
+
+func mustInt(s string) int {
+	if v, err := strconv.Atoi(s); err != nil {
+		panic(fmt.Sprint(s, err))
+	} else {
+		return v
+	}
+}
