69e0872edf76d37bc69c46f11862fae3a9e8ab86
John Verrone <johnverrone@fullstory.com>
2024-12-06 22:07:19 -0700

    more code reorganizing

--- 2024/day06/main.go
+++ 2024/day06/main.go
@@ -33,6 +33,20 @@ const (
 	LEFT
 )
 
+func (d Dir) String() string {
+	switch d {
+	case UP:
+		return "UP"
+	case RIGHT:
+		return "RIGHT"
+	case DOWN:
+		return "DOWN"
+	case LEFT:
+		return "LEFT"
+	}
+	return "INVALID"
+}
+
 type point struct {
 	r, c int
 }
@@ -56,8 +70,11 @@ func NewGuard(r, c int) *Guard {
 
 func (g *Guard) move() {
 	for {
-		newPoint := point{g.r, g.c}
-		for g.canMove() {
+		var newPoint point
+		if g.isDone() {
+			break
+		}
+		if g.canMove() {
 			//check if we can block
 			g.canBlock()
 			// move
@@ -73,20 +90,18 @@ func (g *Guard) move() {
 			}
 			// mark visited
 			newPoint = point{g.r, g.c}
-			fmt.Printf("moving to %v\n", newPoint)
+			fmt.Printf("moving %s to %v\n", g.dir, newPoint)
+			g.visited[newPoint] = append(g.visited[newPoint], g.dir)
+		} else {
+			// turn
+			if g.dir >= 4 {
+				g.dir = 1
+			} else {
+				g.dir++
+			}
+			// add turn direction to visited
 			g.visited[newPoint] = append(g.visited[newPoint], g.dir)
 		}
-		// check bounds & turn
-		if (g.dir == UP && g.r == 0) || (g.dir == DOWN && g.r == len(lab)-1) || (g.dir == RIGHT && g.c == len(lab[0])-1) || (g.dir == LEFT && g.c == 0) {
-			break
-		}
-		if g.dir >= 4 {
-			g.dir = 1
-		} else {
-			g.dir++
-		}
-		// add turn direction to visited
-		g.visited[newPoint] = append(g.visited[newPoint], g.dir)
 	}
 }
 
@@ -138,16 +153,30 @@ func getBlockPoint(dir Dir, g *Guard) point {
 	panic("unhandled direction")
 }
 
+func (g *Guard) isDone() bool {
+	switch g.dir {
+	case UP:
+		return g.r-1 < 0
+	case LEFT:
+		return g.c-1 < 0
+	case RIGHT:
+		return g.c+1 > len(lab[0])-1
+	case DOWN:
+		return g.r+1 > len(lab)-1
+	}
+	panic("unhandled move")
+}
+
 func (g *Guard) canMove() bool {
 	switch g.dir {
 	case UP:
-		return g.r-1 >= 0 && lab[g.r-1][g.c] != '#'
+		return lab[g.r-1][g.c] != '#'
 	case LEFT:
-		return g.c-1 >= 0 && lab[g.r][g.c-1] != '#'
+		return lab[g.r][g.c-1] != '#'
 	case RIGHT:
-		return g.c+1 <= len(lab[0])-1 && lab[g.r][g.c+1] != '#'
+		return lab[g.r][g.c+1] != '#'
 	case DOWN:
-		return g.r+1 <= len(lab)-1 && lab[g.r+1][g.c] != '#'
+		return lab[g.r+1][g.c] != '#'
 	}
 	panic("unhandled move")
 }
@@ -155,11 +184,10 @@ func (g *Guard) canMove() bool {
 func main() {
 	in := util.ParseInput("")
 	in = strings.TrimSpace(in)
-	lines := strings.Split(in, "\n")
-	lab = lines
+	lab = strings.Split(in, "\n")
 
 	var g *Guard
-	for r, row := range lines {
+	for r, row := range lab {
 		c := strings.Index(row, "^")
 		if c >= 0 {
 			fmt.Printf("starting loc %d, %d\n", r, c)
