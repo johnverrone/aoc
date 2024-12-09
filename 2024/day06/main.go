7785edc663121c3119ad1b0f1411d3d8caf5c5ec
John Verrone <john.verrone@gmail.com>
2024-12-08 22:12:47 -0700

    day6 pt2 finally

--- 2024/day06/main.go
+++ 2024/day06/main.go
@@ -21,7 +21,7 @@ const sample = `
 ......#...
 `
 
-var lab []string
+var lab [][]byte
 
 type Dir int
 
@@ -52,46 +52,55 @@ type point struct {
 }
 
 type Guard struct {
-	r, c        int
-	dir         Dir
-	visited     map[point][]Dir
-	blockPoints []point
+	start   point
+	loc     point
+	dir     Dir
+	visited map[point][]Dir
 }
 
-func NewGuard(r, c int) *Guard {
+func NewGuard(p point) *Guard {
 	return &Guard{
-		r:           r,
-		c:           c,
-		dir:         UP,
-		visited:     map[point][]Dir{{r, c}: {UP}},
-		blockPoints: []point{},
+		start:   p,
+		loc:     p,
+		dir:     UP,
+		visited: map[point][]Dir{p: {UP}},
 	}
 }
 
-func (g *Guard) move() {
+type result int
+
+const (
+	DONE = result(iota)
+	LOOP
+)
+
+func (g *Guard) move() result {
+	// reset
+	g.loc = g.start
+	g.visited = map[point][]Dir{}
+	g.dir = UP
 	for {
-		var newPoint point
 		if g.isDone() {
-			break
+			return DONE
 		}
 		if g.canMove() {
-			//check if we can block
-			g.canBlock()
+			if dir := g.visited[g.nextPoint()]; slices.Contains(dir, g.dir) {
+				return LOOP
+			}
 			// move
 			switch g.dir {
 			case UP:
-				g.r--
+				g.loc.r--
 			case LEFT:
-				g.c--
+				g.loc.c--
 			case RIGHT:
-				g.c++
+				g.loc.c++
 			case DOWN:
-				g.r++
+				g.loc.r++
 			}
 			// mark visited
-			newPoint = point{g.r, g.c}
-			fmt.Printf("moving %s to %v\n", g.dir, newPoint)
-			g.visited[newPoint] = append(g.visited[newPoint], g.dir)
+			// fmt.Printf("moving %s to %v\n", g.dir, g.loc)
+			g.visited[g.loc] = append(g.visited[g.loc], g.dir)
 		} else {
 			// turn
 			if g.dir >= 4 {
@@ -100,55 +109,22 @@ func (g *Guard) move() {
 				g.dir++
 			}
 			// add turn direction to visited
-			g.visited[newPoint] = append(g.visited[newPoint], g.dir)
+			// fmt.Printf("turning, adding %d to visited point at %d, %d\n", g.dir, newPoint.r, newPoint.c)
+			g.visited[g.loc] = append(g.visited[g.loc], g.dir)
 		}
 	}
 }
 
-func (g *Guard) canBlock() {
-	yes := false
+func (g *Guard) nextPoint() point {
 	switch g.dir {
 	case UP:
-		for i := g.c + 1; i <= len(lab[0])-1 && lab[g.r][i] != '#'; i++ {
-			vDir, ok := g.visited[point{g.r, i}]
-			yes = ok && slices.Contains(vDir, RIGHT)
-		}
+		return point{g.loc.r - 1, g.loc.c}
 	case LEFT:
-		for i := g.r - 1; i >= 0 && lab[i][g.c] != '#'; i-- {
-			vDir, ok := g.visited[point{i, g.c}]
-			yes = ok && slices.Contains(vDir, UP)
-		}
+		return point{g.loc.r, g.loc.c - 1}
 	case RIGHT:
-		for i := g.r + 1; i <= len(lab)-1 && lab[i][g.c] != '#'; i++ {
-			vDir, ok := g.visited[point{i, g.c}]
-			yes = ok && slices.Contains(vDir, DOWN)
-		}
+		return point{g.loc.r, g.loc.c + 1}
 	case DOWN:
-		for i := g.c - 1; i >= 0 && lab[g.r][i] != '#'; i-- {
-			vDir, ok := g.visited[point{g.r, i}]
-			yes = ok && slices.Contains(vDir, LEFT)
-		}
-	}
-	if yes {
-		blockPoint := getBlockPoint(g.dir, g)
-		// fmt.Printf("can block move at: %v\n", blockPoint)
-		// g.print(blockPoint)
-		if !slices.Contains(g.blockPoints, blockPoint) {
-			g.blockPoints = append(g.blockPoints, blockPoint)
-		}
-	}
-}
-
-func getBlockPoint(dir Dir, g *Guard) point {
-	switch dir {
-	case UP:
-		return point{g.r - 1, g.c}
-	case LEFT:
-		return point{g.r, g.c - 1}
-	case RIGHT:
-		return point{g.r, g.c + 1}
-	case DOWN:
-		return point{g.r + 1, g.c}
+		return point{g.loc.r + 1, g.loc.c}
 	}
 	panic("unhandled direction")
 }
@@ -156,13 +132,13 @@ func getBlockPoint(dir Dir, g *Guard) point {
 func (g *Guard) isDone() bool {
 	switch g.dir {
 	case UP:
-		return g.r-1 < 0
+		return g.loc.r-1 < 0
 	case LEFT:
-		return g.c-1 < 0
+		return g.loc.c-1 < 0
 	case RIGHT:
-		return g.c+1 > len(lab[0])-1
+		return g.loc.c+1 > len(lab[0])-1
 	case DOWN:
-		return g.r+1 > len(lab)-1
+		return g.loc.r+1 > len(lab)-1
 	}
 	panic("unhandled move")
 }
@@ -170,13 +146,13 @@ func (g *Guard) isDone() bool {
 func (g *Guard) canMove() bool {
 	switch g.dir {
 	case UP:
-		return lab[g.r-1][g.c] != '#'
+		return lab[g.loc.r-1][g.loc.c] != '#'
 	case LEFT:
-		return lab[g.r][g.c-1] != '#'
+		return lab[g.loc.r][g.loc.c-1] != '#'
 	case RIGHT:
-		return lab[g.r][g.c+1] != '#'
+		return lab[g.loc.r][g.loc.c+1] != '#'
 	case DOWN:
-		return lab[g.r+1][g.c] != '#'
+		return lab[g.loc.r+1][g.loc.c] != '#'
 	}
 	panic("unhandled move")
 }
@@ -184,35 +160,61 @@ func (g *Guard) canMove() bool {
 func main() {
 	in := util.ParseInput("")
 	in = strings.TrimSpace(in)
-	lab = strings.Split(in, "\n")
+	lines := strings.Split(in, "\n")
 
-	var g *Guard
-	for r, row := range lab {
-		c := strings.Index(row, "^")
-		if c >= 0 {
-			fmt.Printf("starting loc %d, %d\n", r, c)
-			g = NewGuard(r, c)
+	var start point
+	lab = make([][]byte, len(lines))
+	for r, line := range lines {
+		for c, ch := range line {
+			if ch == '^' {
+				start = point{r, c}
+			}
+			lab[r] = append(lab[r], byte(ch))
 		}
 	}
 
+	var g *Guard
+	fmt.Printf("starting loc %v\n", start)
+	g = NewGuard(start)
 	g.move()
 
 	// part 1
 	fmt.Printf("Part 1: %d\n", len(g.visited))
 
 	// part 2
-	fmt.Printf("Part 2: %d\n", len(g.blockPoints))
+	sum := 0
+	for r := 0; r < len(lab); r++ {
+		for c := 0; c < len(lab[0]); c++ {
+			p := point{r, c}
+			if lab[r][c] == '.' && p != g.start {
+				lab[r][c] = '#'
+
+				res := g.move()
+				if res == LOOP {
+					sum++
+				}
+
+				lab[r][c] = '.'
+			}
+		}
+	}
+	fmt.Printf("Part 2: %d\n", sum)
 }
 
-func (g *Guard) print(bp point) {
+func (g *Guard) print() {
 	for r, row := range lab {
 		for c, col := range row {
-			if c == bp.c && r == bp.r {
-				fmt.Print("0")
-				continue
-			}
-			if r == g.r && c == g.c {
-				fmt.Print("^")
+			if r == g.loc.r && c == g.loc.c {
+				switch g.dir {
+				case UP:
+					fmt.Print("^")
+				case DOWN:
+					fmt.Print("v")
+				case LEFT:
+					fmt.Print("<")
+				case RIGHT:
+					fmt.Print(">")
+				}
 			} else if dir, ok := g.visited[point{r, c}]; ok {
 				if len(dir) > 1 {
 					fmt.Print("+")
